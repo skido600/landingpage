9dc52a555fb043c82a8c98f92a853305dc4e9982
Chuks <63956312+skido600@users.noreply.github.com>
2025-01-20 04:13:46 +0100

    nav dishes favourfly

--- src/Components/Dishes.jsx
+++ src/Components/Dishes.jsx
@@ -1,4 +1,28 @@
 function Dishes() {
+  const dishes = [
+    {
+      title: "Avacado Nacho",
+      Description: "   Description of the item",
+      img: "/svg/ff.svg",
+      price: 10.0,
+      rating: 4.9,
+    },
+    {
+      title: "Best Seller Nachos",
+      Description: "   Description of the item",
+      img: "/svg/best-seller.svg",
+      price: 22.0,
+      rating: 4.9,
+    },
+    {
+      title: "Original Nachos",
+      Description: "   Description of the item",
+      img: "/svg/original.svg",
+      price: 22.0,
+      rating: 4.9,
+    },
+  ];
+
   return (
     <>
       <main>
@@ -6,12 +30,24 @@ function Dishes() {
           <h1 className="text-[#8F0000] text-[30px] lg:text-[60px] font-Prosto">
             Best Dishes From Our Menu{" "}
           </h1>
-          <div>
-            <article>
-              <img src="/svg/ff.svg" alt="logo" />
-              <h1>Avacado Nacho</h1>
-              <p className="text-[#555555]">Description of the item</p>
-            </article>
+          <div className="grid md:grid-cols-3 space-y-4  gap-x-4">
+            {dishes.map((item, key) => (
+              <article key={key} className="bg-white  px-4 rounded-lg">
+                <img src={item.img} alt={item.title} className="w-[]" />
+                <h1>{item.title}</h1>
+                <p className="text-[#555555] font-inter">{item.Description}</p>
+                <div className="flex justify-between items-center">
+                  <p>
+                    <span>$</span>
+                    {item.price}
+                  </p>
+                  <div>
+                    <img src="/svg/fi-sr-star.svg" className="w-4" alt="" />
+                    <p className="text-[#555555]">{item.rating}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
           </div>
         </section>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       fontFamily: {
         Prosto: ["Prosto One", "serif"],
         jost: ["Jost", "serif"],
+        inter: ["Inter", "serif"],
       },
       backgroundImage: {
         "nachos-bg": "url('/svg/pexels-willoworld-3768012.jpg') ",
